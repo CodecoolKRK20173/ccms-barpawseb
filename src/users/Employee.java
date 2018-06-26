158d5e70eca870566e288ad2dac1c6400ce99749
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-06-26 13:21:41 +0200

    add setStudentsList()

--- src/users/Employee.java
+++ src/users/Employee.java
@@ -7,12 +7,16 @@ public class Employee extends User{
     protected static List<Student> allStudents;
 
     protected Employee(String name){
-        super(name);   
+        super(name);
     }
 
-    protected Employee (String name, String password, int id, List<Student> allStudents){
+    protected Employee (String name, String password, int id, ArrayList<Student> allStudents){
         super(name, password, id);
         this.allStudents = allStudents;
+    }
+
+    public void setStudentsList(ArrayList<Student> students){
+        allStudents = students;
     }
 
     public List<Student> getAllStudents(){
