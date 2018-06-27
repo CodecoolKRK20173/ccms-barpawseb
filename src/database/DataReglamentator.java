a320ffaa62c3e67ceb67df36831e0645bca44cf0
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-06-28 01:23:06 +0200

    add UpdateDataManager and supporting method; add getUser methods

--- src/database/DataReglamentator.java
+++ src/database/DataReglamentator.java
@@ -1,18 +1,17 @@
 package database;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-
 import users.*;
 
 public class DataReglamentator{
-    Iterable<String[]> dataManager;
+    DataManager dataManager;
     List<Student> students =  new ArrayList<>();
     List<Employee> employees = new ArrayList<>();
-    Collection<User> allUsers;
+    Collection <User> allUsers;
 
     public DataReglamentator() throws IOException{
         dataManager = new DataManager();
@@ -35,6 +34,20 @@ public class DataReglamentator{
             if(line[3].equals("e") || line[3].equals("m"))
                 employees.add(new Employee(line[1], line[0], line[2], Double.parseDouble(line[4])));
         }
+    }
+ 
+    public User getUserByName(String name)throws NoSuchElementException{
+        for( User user: allUsers){
+            if (user.getName().equals(name)) return user;
+        }
+        throw new NoSuchElementException();
+    }
+
+    public User getUserByEmail(String email) throws NoSuchElementException{
+        for (User user: allUsers){
+            if (user.getEmail().equals(email)) return user;
+        }
+        throw new NoSuchElementException();
     }
 
     public Student getStudentByName(String name)throws NoSuchElementException{
@@ -106,12 +119,50 @@ public class DataReglamentator{
         employees.add(new Employee(name, email));
     }
 
-  /* public void updateDataManager(List<User> allUsers){
-       List<String[]> rawData = new ArrayList<>();
-       for (User user : allUsers){
-           String[] line = {}
-       }
-  }*/
+   
+    public void updateDataManager(){
+        List <String[]> lines = transformUsersListToExportLines();
+        dataManager.setUsersLines(lines);
+    }
+
+    private List<String[]> transformUsersListToExportLines(){
+        List<String[]> usersLines = new ArrayList<>();
+        String status;
+        String salary;
+        String grades;
+        for (User user: allUsers){
+            if(user.getStatus().equals("Student")){
+                Student student = (Student)user;
+                status = "s";
+                salary = "null";
+                grades = String.valueOf(student.getGradesSum());
+            }
+            if (user.getStatus().equals("Mentor")){
+                Mentor mentor = (Mentor) user;
+                status = "m";
+                salary = String.valueOf(mentor.getSalary());
+                grades = "null";
+
+            }
+            if(user.getStatus().equals("Employee")){
+                Employee employee = (Employee)user;
+                status = "e";
+                salary = String.valueOf(employee.getSalary());
+                grades = "null";
+            }
+            else{
+                Supervisor supervisor = (Supervisor) user;
+                status = "b";
+                grades = "null";
+                salary = "null";
+            }
+            String[] line = {user.getEmail(),user.getName(),user.getPassword(),status,grades,salary};
+            usersLines.add(line);
+        }
+        return usersLines;        
+    }
+           
+  
 
     public static void main(String[] args)throws IOException {
         DataReglamentator test = new DataReglamentator();
