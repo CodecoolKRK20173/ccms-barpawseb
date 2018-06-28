c0ce9257b12e6db55b8b9a6503215587d609e4c2
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-06-28 23:41:01 +0200

    add filepath as paramether to constructor

--- src/database/DataManager.java
+++ src/database/DataManager.java
@@ -10,18 +10,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DataManager implements Iterable<String[]> {
-    private List<String[]> usersLines = new ArrayList<>();
-    private final String usersDbLocation = "src/database/users.csv";
+    private List<String[]> dataLines = new ArrayList<>();
+    private final String filePath;
 
-    public DataManager() throws IOException {
-        loadUsersList(usersDbLocation);
+    public DataManager(String filePath) throws IOException {
+        this.filePath = filePath;
+        loadDataList(filePath);
     }
 
-    private void loadUsersList(String fileName) throws IOException {
+    private void loadDataList(String fileName) throws IOException {
         Scanner scan = new Scanner(Paths.get(fileName));
         while (scan.hasNextLine()) {
-            String[] userDetails = scan.nextLine().split(",");
-            usersLines.add(userDetails);
+            String[] data = scan.nextLine().split(",");
+            dataLines.add(data);
         }
     }
 
@@ -39,7 +40,7 @@ public class DataManager implements Iterable<String[]> {
         }
         String text = sb.toString();
 
-        try (PrintWriter out = new PrintWriter(usersDbLocation)) {
+        try (PrintWriter out = new PrintWriter(filePath)) {
             out.println(text);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -47,10 +48,11 @@ public class DataManager implements Iterable<String[]> {
     }
 
     public Iterator<String[]> iterator() {
-        return usersLines.iterator();
+        return dataLines.iterator();
     }
 
     public void setUsersLines(List<String[]> lines){ //overwrites list with content to be written to data base;
-        usersLines = lines;
+        dataLines = lines;
+        saveListToDatabase(dataLines);
     }
 }
