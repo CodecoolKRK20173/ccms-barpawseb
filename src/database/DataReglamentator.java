1730b2bb9d87de469b9b92994946c15318985267
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-06-28 15:13:57 +0200

    add extractMentors, allusersUpdater; total refactor

--- src/database/DataReglamentator.java
+++ src/database/DataReglamentator.java
@@ -4,38 +4,70 @@ import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.Arrays;
+
 import java.io.IOException;
 import users.*;
 
 public class DataReglamentator{
     DataManager dataManager;
+    List<Supervisor> supervisors = new ArrayList<>();
     List<Student> students =  new ArrayList<>();
     List<Employee> employees = new ArrayList<>();
-    Collection <User> allUsers;
+    List <User> allUsers = new ArrayList<>();
 
     public DataReglamentator() throws IOException{
         dataManager = new DataManager();
-        createStudentsList();
-        createEmployeesList();
+        createListsOfAllTypeOfUser();
+        updateAllUssersList();
+    }
+
+    private void updateAllUssersList(){
+        allUsers.clear();
+        allUsers.addAll(supervisors);
         allUsers.addAll(students);
         allUsers.addAll(employees);
     }
 
-
-    private void createStudentsList(){
+    private void createListsOfAllTypeOfUser(){
         for(String[] line: dataManager){
-            if(line[3].equals("s"))
-                students.add(new Student(line[1], line[0], line[2], Integer.parseInt(line[4])));
+            String email = line[0];
+            String name = line[1];
+            String password = line[2];
+            String status = line[3];
+            String absence = line[4];
+            String salary = line[5];
+            if(status.equals("Supervisor"))
+                supervisors.add(new Supervisor(name, email, password));
+            else if(status.equals("Student"))
+                students.add(new Student(name, email, password, Integer.parseInt(absence)));
+            else employees.add(new Employee(name, email, password, Double.parseDouble(salary)));
         }
     }
-
-    private void createEmployeesList(){ 
-        for(String[] line: dataManager){
-            if(line[3].equals("e") || line[3].equals("m"))
-                employees.add(new Employee(line[1], line[0], line[2], Double.parseDouble(line[4])));
-        }
+    
+    public Collection<User> getUsersList(){
+        return allUsers;
     }
+    
+    public List<Student> getStudentsList(){
+        return students;
+    }
+
+    public List<Employee> getEmployeesList(){
+        return employees;
+    }
+
+    public List<Employee> extractMentorsList(){
+        List<Employee> mentorsPicked = new ArrayList<>();
+        for(Employee employee: employees){
+            if(employee.getStatus().equals("Mentor"))
+                mentorsPicked.add(employee);
+        }
+        return mentorsPicked;
+    }
+
  
+    //usefull only during login
     public User getUserByName(String name)throws NoSuchElementException{
         for( User user: allUsers){
             if (user.getName().equals(name)) return user;
@@ -43,6 +75,7 @@ public class DataReglamentator{
         throw new NoSuchElementException();
     }
 
+    //usefull only during login
     public User getUserByEmail(String email) throws NoSuchElementException{
         for (User user: allUsers){
             if (user.getEmail().equals(email)) return user;
@@ -79,84 +112,71 @@ public class DataReglamentator{
         throw new NoSuchElementException();
     }
 
-    public List<Student> getStudentsList(){
-        return students;
-    }
-
-    public List<Employee> getEmployees(){
-        return employees;
-    }
-
     public void addStudent(String name, String email){
         students.add(new Student(name,email));
+        updateAllUssersList();
     }
 
     public void removeStudentByEmail(String email ){
         students.removeIf(s -> s.getEmail().equals(email));
+        updateAllUssersList();
     }
 
     public void removeStudent(Student student){
         students.remove(student);
+        updateAllUssersList();
     }
 
+    
     public void addMentor(String name, String email){
         employees.add(new Mentor(name, email));
+        updateAllUssersList();
     }
 
     public void addSimpleEmployee(String name, String email){
         employees.add(new Employee(name, email));
+        updateAllUssersList();
     }
 
     public void removeEmployeeByEmail(String email){
         employees.removeIf(emp -> emp.getEmail().equals(email));
+        updateAllUssersList();
     }
     
     public void removeEmployee(Employee employee){
         employees.remove(employee);
-    }
-
-    public void addEmployee(String name, String email){
-        employees.add(new Employee(name, email));
+        updateAllUssersList();
     }
 
    
     public void updateDataManager(){
         List <String[]> lines = transformUsersListToExportLines();
         dataManager.setUsersLines(lines);
+        for(String[] line:lines)
+            System.out.println(Arrays.toString(line));
     }
 
     private List<String[]> transformUsersListToExportLines(){
+        updateAllUssersList();
         List<String[]> usersLines = new ArrayList<>();
-        String status;
         String salary;
-        String grades;
+        String absence;
         for (User user: allUsers){
             if(user.getStatus().equals("Student")){
                 Student student = (Student)user;
-                status = "s";
                 salary = "null";
-                grades = String.valueOf(student.getGradesSum());
+                absence = String.valueOf(student.getAbsence());
             }
-            if (user.getStatus().equals("Mentor")){
-                Mentor mentor = (Mentor) user;
-                status = "m";
-                salary = String.valueOf(mentor.getSalary());
-                grades = "null";
-
-            }
-            if(user.getStatus().equals("Employee")){
-                Employee employee = (Employee)user;
-                status = "e";
-                salary = String.valueOf(employee.getSalary());
-                grades = "null";
+            else if (user.getStatus().equals("Supervisor")){
+                absence = "null";
+                salary = "null";
             }
             else{
-                Supervisor supervisor = (Supervisor) user;
-                status = "b";
-                grades = "null";
-                salary = "null";
+            Employee employee = (Employee)user;
+                salary = String.valueOf(employee.getSalary());
+                absence = "null";
             }
-            String[] line = {user.getEmail(),user.getName(),user.getPassword(),status,grades,salary};
+            String[] line = {user.getEmail(),user.getName(),user.getPassword(),user.getStatus(),absence,salary};
             usersLines.add(line);
         }
         return usersLines;        
@@ -165,9 +185,20 @@ public class DataReglamentator{
   
 
     public static void main(String[] args)throws IOException {
+        User student = new Student("name", "email");
+        System.out.println(student.getStatus());
         DataReglamentator test = new DataReglamentator();
+        test.updateDataManager();
+       /*
         System.out.println(test.getStudentsList());
-        System.out.println(test.getEmployees());
+        System.out.println(test.getEmployeesList());
+        System.out.println(test.getUsersList());
+        System.out.println(" \n* * * \n");
+
+        test.addStudent("piotr", "email");
+        System.out.println(test.getStudentsList());
+        System.out.println(" \n* * * \n");*/
+        System.out.println(test.getUsersList());
     }
 }
 
