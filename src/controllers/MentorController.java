7ce3c2942ed3a337d8c57197b77af01c38914c7a
nomideusz <b.dymet@gmail.com>
2018-06-29 09:07:34 +0200

    Add exception for handlin emails in MentorController. Minor cosmetic changes

--- src/controllers/MentorController.java
+++ src/controllers/MentorController.java
@@ -1,4 +1,5 @@
 package controllers;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.List;
 import database.*;
@@ -9,7 +10,7 @@ public class MentorController {
     
     private Mentor mentor;
     private View view;
-    private DataReglamentator database;
+    private DataReglamentator dataReglamentator;
     private final String[] namu = {"Show all Students", "Add Student",
                                    "Remove Student", "View Student's Data",
                                    "Check Attendance Of Students",
@@ -20,46 +21,53 @@ public class MentorController {
 
     public MentorController(DataReglamentator database, String email){
         view = new View();
-        this.database = database;
-        mentor = (Mentor) this.database.getEmployeeByEmail(email);
+        this.dataReglamentator = database;
+        mentor = (Mentor) this.dataReglamentator.getEmployeeByEmail(email);
     }
+
     public void setPassword(){
         System.out.println("Your current password: " + mentor.getPassword());
         System.out.print("Enter new password: ");
         String newPassword = in.nextLine();
         mentor.setPassword(newPassword);
-        database.updateDataManager();
+        dataReglamentator.updateDataManager();
     }
+
     public List<Student> getStudentsList(){
-        return database.getStudentsList();
+        return dataReglamentator.getStudentsList();
     }
+
     public void showStudents(){
         view.showStudentList(getStudentsList());
     }
+
     public void showManu(){
         view.showManu(this.namu);
     }
+
     public void addStudent(){
         System.out.print("Enter student's name: ");
         String name = in.nextLine();
         System.out.print("Enter student's email: ");
         String email = in.nextLine();;
-        database.addStudent(name, email);
-        database.updateDataManager();
+        dataReglamentator.addStudent(name, email);
+        dataReglamentator.updateDataManager();
     }
+
     public void removeStudent(){
         System.out.print("Enter student's email You want to remove: ");
         String email = in.nextLine();
-        if (database.removeStudentByEmail(email))
+        if (dataReglamentator.removeStudentByEmail(email))
             System.out.println("Student has been removed.");
         else
             System.out.println("There's no such student.");
-        database.updateDataManager();
+        dataReglamentator.updateDataManager();
     }
+
     public void showStudentsData(){
         System.out.print("Enter student's email for editing student's data: ");
         String email = in.nextLine();
-        Student student = database.getStudentByEmail(email);
+        Student student = dataReglamentator.getStudentByEmail(email);
         view.showStudentData(student);
     }
 
@@ -71,22 +79,31 @@ public class MentorController {
             if (in.nextLine().toLowerCase().equals("n"))
                 student.incrementAbsence();
         }
-        database.updateDataManager();
+        dataReglamentator.updateDataManager();
     }
+
     public void gradeStudent(){
-        System.out.print("Enter student's email You want to grade: ");
-        String email = in.nextLine();
-        Student student = database.getStudentByEmail(email);
-        view.showStudentData(student);
-        view.showGrades(database.getStudentByEmail(email).getGrades());
+        boolean isEmailInDatabase = false;
+        Student student = null;
+        while (!isEmailInDatabase) {
+            try {
+                System.out.print("Enter student's email you want to grade: ");
+                String email = in.nextLine();
+                student = dataReglamentator.getStudentByEmail(email);
+                view.showStudentData(student);
+                view.showGrades(dataReglamentator.getStudentByEmail(email).getGrades());
+                isEmailInDatabase = true;
+            }
+            catch (NoSuchElementException e) {
+                System.out.println("There is no such e-mail.");
+            }
+        }
+
         System.out.print("Choose assignment You want to grade: ");
         String assignment = in.nextLine();
         System.out.println("Enter Your grade value: ");
         double grade = Double.parseDouble(in.nextLine());
         student.getGrades().put(assignment, grade);
-        database.updateDataManager();
+        dataReglamentator.updateDataManager();
     }
-
-
-
 }
